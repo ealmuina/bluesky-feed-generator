0f9928ae8a6dd2859081254926023989b4772997
Eddy Almuiña <almuina.e@gmail.com>
2023-10-27 18:55:44 +0200

    Fix HOSTNAME env variable

--- server/config.py
+++ server/config.py
@@ -1,10 +1,10 @@
 import os
 
 SERVICE_DID = os.environ.get('SERVICE_DID', None)
-HOSTNAME = os.environ.get('HOSTNAME', None)
+HOSTNAME = os.environ.get('BSKY_HOSTNAME', None)
 
 if HOSTNAME is None:
-    raise RuntimeError('You should set "HOSTNAME" environment variable first.')
+    raise RuntimeError('You should set "BSKY_HOSTNAME" environment variable first.')
 
 if SERVICE_DID is None:
     SERVICE_DID = f'did:web:{HOSTNAME}'
