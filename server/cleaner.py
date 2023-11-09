7269c42461be8ed8fed6fd26debac912507c6212
Eddy Almuiña <almuina.e@gmail.com>
2023-11-09 20:52:09 +0100

    Fix cleaner code

--- server/cleaner.py
+++ server/cleaner.py
@@ -7,7 +7,7 @@ from server.database import Post
 def run(stop_event=None):
     while stop_event is None or not stop_event.is_set():
         now = datetime.datetime.now()
-        Post.delete().where(Post.indexed_at <= now - datetime.timedelta(days=7))
+        Post.delete().where(Post.indexed_at <= now - datetime.timedelta(days=7)).execute()
 
         time.sleep(86400)  # 1 day
         continue
