71f2412a04870bb51b41c8ebf4a919600d0e4c7d
Eddy Almuiña <almuina.e@gmail.com>
2024-02-17 13:17:12 +0100

    Interactions cleanup

--- server/tasks/cleaner.py
+++ server/tasks/cleaner.py
@@ -1,13 +1,14 @@
 import datetime
 import time
 
-from server.database import Post
+from server.database import Post, Interaction
 
 
 def run(stop_event=None):
     while stop_event is None or not stop_event.is_set():
         now = datetime.datetime.now()
         Post.delete().where(Post.indexed_at <= now - datetime.timedelta(days=7)).execute()
+        Interaction.delete().where(Interaction.indexed_at <= now - datetime.timedelta(days=7)).execute()
 
         time.sleep(86400)  # 1 day
         continue
